1a37e32b91f64d2fcedddc046ce914259fd5cf9b
Joshua French <jfrench@mines.edu>
2012-09-07 19:10:58 -0600

    - Replaced tabs with ViewFlipper and flipping button with text "You   forget I'm good at backflips. FLIPPY FLIP!"

--- LunchList/src/csci498/lunchlist/LunchList.java
+++ LunchList/src/csci498/lunchlist/LunchList.java
@@ -3,10 +3,8 @@ package csci498.lunchlist;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.app.TabActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
-import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
@@ -21,12 +19,13 @@ import android.widget.ListView;
 import android.widget.RadioGroup;
 import android.widget.TabHost;
 import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 /**
  * The LunchList activity provides a form for users to enter and store
  * restaurant information and displays that information in a list
  */
-public class LunchList extends TabActivity {
+public class LunchList extends Activity {
 
 	private List<Restaurant> model                   = new ArrayList<Restaurant>();
 	private RestaurantAdapter adapter                = null;
@@ -34,6 +33,7 @@ public class LunchList extends TabActivity {
 	private EditText name                            = null;
 	private EditText address                         = null;
 	private RadioGroup types                         = null;
+	private ViewFlipper flipper                      = null;
     
 	private View.OnClickListener onSave = new View.OnClickListener() {
 		
@@ -62,6 +62,14 @@ public class LunchList extends TabActivity {
 		
 	};
 	
+	private View.OnClickListener onFlip = new View.OnClickListener() {
+		
+		@Override
+		public void onClick(View arg0) {
+			flipper.showNext();
+		}
+	};
+	
 	private AdapterView.OnItemClickListener onListClick = new AdapterView.OnItemClickListener() {
 		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 			Restaurant r = model.get(position);
@@ -78,8 +86,6 @@ public class LunchList extends TabActivity {
 			else {
 				types.check(R.id.delivery);
 			}
-			
-			getTabHost().setCurrentTab(1);
 		}
     };
 	
@@ -150,6 +156,7 @@ public class LunchList extends TabActivity {
 		name    = (EditText) findViewById(R.id.name);
 		address = (EditText) findViewById(R.id.addr);
 		types   = (RadioGroup) findViewById(R.id.types);
+		flipper = (ViewFlipper) findViewById(R.id.flipper);
         
         AutoCompleteTextView addressView = (AutoCompleteTextView) findViewById(R.id.addr);
         addressSuggestions = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line);
@@ -158,25 +165,13 @@ public class LunchList extends TabActivity {
         Button save = (Button) findViewById(R.id.save);
         save.setOnClickListener(onSave);
         
+        Button flip = (Button) findViewById(R.id.flip);
+        flip.setOnClickListener(onFlip);
+        
         ListView list = (ListView) findViewById(R.id.restaurants);
         adapter       = new RestaurantAdapter();
         list.setAdapter(adapter);
         list.setOnItemClickListener(onListClick);
-        
-        TabHost.TabSpec spec = getTabHost().newTabSpec("tag1");
-        
-        spec.setContent(R.id.restaurants);
-        spec.setIndicator("List", getResources().getDrawable(R.drawable.list));
-        
-        getTabHost().addTab(spec);
-        
-        spec = getTabHost().newTabSpec("tag2");
-        spec.setContent(R.id.details);
-        spec.setIndicator("Details", getResources().getDrawable(R.drawable.restaurant));
-        
-        getTabHost().addTab(spec);
-        
-        getTabHost().setCurrentTab(0);
     }
     
     @Override
