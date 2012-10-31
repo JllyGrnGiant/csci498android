11e0d580a93e390dac7c340cf6d3003c251ee3ce
Joshua French <jfrench@mines.edu>
2012-10-31 02:20:41 -0600

    APT18
    - Added marker image
    - Set marker/overlay in RestaurantMap

--- LunchList/src/csci498/lunchlist/RestaurantMap.java
+++ LunchList/src/csci498/lunchlist/RestaurantMap.java
@@ -1,19 +1,64 @@
 package csci498.lunchlist;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.ItemizedOverlay;
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+import com.google.android.maps.OverlayItem;
 
 public class RestaurantMap extends MapActivity {
 
 	public static final String EXTRA_LATITUDE="apt.tutorial.EXTRA_LATITUDE";
 	public static final String EXTRA_LONGITUDE="apt.tutorial.EXTRA_LONGITUDE";
 	public static final String EXTRA_NAME="apt.tutorial.EXTRA_NAME";
+
+	private MapView map;
+	
+	private class RestaurantOverlay extends ItemizedOverlay<OverlayItem> {
+		
+		private OverlayItem item;
+		
+		public RestaurantOverlay(Drawable marker, GeoPoint point, String name) {
+			super(marker);
+			boundCenterBottom(marker);
+			item = new OverlayItem(point, name, name);
+			populate();
+		}
+		
+		@Override
+		protected OverlayItem createItem(int i) {
+			return item;
+		}
+		
+		@Override
+		public int size() {
+			return 1;
+		}
+	}
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.map);
+		
+		double latitude  = getIntent().getDoubleExtra(EXTRA_LATITUDE, 0);
+		double longitude = getIntent().getDoubleExtra(EXTRA_LONGITUDE, 0);
+	
+		map = (MapView) findViewById(R.id.map);
+		map.getController().setZoom(17);
+		
+		GeoPoint status = new GeoPoint((int) (latitude * 1000000), (int) (longitude * 1000000));
+		
+		map.getController().setCenter(status);
+		map.setBuiltInZoomControls(true);
+		
+		Drawable marker = getResources().getDrawable(R.drawable.marker);
+		marker.setBounds(0, 0, marker.getIntrinsicWidth(), marker.getIntrinsicHeight());
+		
+		map.getOverlays().add(new RestaurantOverlay(marker, status, getIntent().getStringExtra(EXTRA_NAME)));
 	}
 	
 	@Override
