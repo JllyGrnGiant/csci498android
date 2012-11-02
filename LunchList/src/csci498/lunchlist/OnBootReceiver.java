f6f4cd78c23b3f0d8d7c6dfa67e9f3f05a9702c8
Joshua French <jfrench@mines.edu>
2012-11-01 22:42:49 -0600

    APT19
    - Added new OnBootReceiver class
    - Modified android manifest to include new boot permission and add a
      receiver

---
+++ LunchList/src/csci498/lunchlist/OnBootReceiver.java
@@ -0,0 +1,13 @@
+package csci498.lunchlist;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class OnBootReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context arg0, Intent arg1) {
+		// TODO something
+	}
+}
