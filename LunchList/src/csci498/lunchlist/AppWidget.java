4573c00f511225734270f049d668bcb02301cfb0
Joshua French <jfrench@mines.edu>
2012-11-11 19:18:02 -0700

    APT22 (cannot tell if it works because emulator is sucktastic)
    - Added ListWidgetService for Honeycomb widget
    - Added ListViewsFactory for Honeycomb widget
    - Added new layouts for Honeycomb widget

--- LunchList/src/csci498/lunchlist/AppWidget.java
+++ LunchList/src/csci498/lunchlist/AppWidget.java
@@ -1,13 +1,45 @@
 package csci498.lunchlist;
 
+import android.annotation.SuppressLint;
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.widget.RemoteViews;
 
 public class AppWidget extends AppWidgetProvider {
 	@Override
 	public void onUpdate(Context ctxt, AppWidgetManager mgr, int[] appWidgetIds) {
-		ctxt.startService(new Intent(ctxt, WidgetService.class));
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			onHCUpdate(ctxt, mgr, appWidgetIds);
+		}
+		else {
+			ctxt.startService(new Intent(ctxt, WidgetService.class));
+		}
+	}
+
+	@SuppressLint({ "NewApi", "NewApi" })
+	@SuppressWarnings("deprecation")
+	private void onHCUpdate(Context ctxt, AppWidgetManager mgr, int[] appWidgetIds) {
+		for (int i = 0; i < appWidgetIds.length; ++i) {
+			Intent svcIntent = new Intent(ctxt, ListWidgetService.class);
+			
+			svcIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]);
+			svcIntent.setData(Uri.parse(svcIntent.toURI()));
+			
+			RemoteViews widget = new RemoteViews(ctxt.getPackageName(), R.layout.widget);
+			widget.setRemoteAdapter(appWidgetIds[i], svcIntent);
+			
+			Intent clickIntent = new Intent(ctxt, DetailForm.class);
+			PendingIntent clickPI = PendingIntent.getActivity(ctxt, 0, clickIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+			
+			widget.setPendingIntentTemplate(R.id.restaurants, clickPI);
+			mgr.updateAppWidget(appWidgetIds, widget);
+		}
+		
+		super.onUpdate(ctxt, mgr, appWidgetIds);
 	}
 }
